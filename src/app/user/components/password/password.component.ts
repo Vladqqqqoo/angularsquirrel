f1a802c8934f3c28b408c31abd5303d79d1c85fa
Sergei <s.belinski.vironit@vironit.ru>
2019-04-25 22:37:33 +0300

    feat: add password form

--- src/app/user/components/password/password.component.ts
+++ src/app/user/components/password/password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ChangePasswordValidator} from './change.password.validator';
 
 @Component({
   selector: 'app-password',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordComponent implements OnInit {
 
-  constructor() { }
+  passwordForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = fb.group({
+      oldPassword: ['', [Validators.required, Validators.pattern(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)]],
+      newPassword: ['',  [Validators.pattern(/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)]],
+    }, {validator: ChangePasswordValidator.confirmPassword});
+  }
+
+  getOldPasswordErrorMessage() {
+    return this.passwordForm.get('oldPassword').hasError('required') ? 'You must enter a value' :
+      this.passwordForm.get('oldPassword').hasError('pattern') ? 'Not a valid first name' :
+        '';
+  }
+
+  getNewPasswordErrorMessage() {
+    return this.passwordForm.get('newPassword').hasError('required') ? 'You must input new password' :
+      this.passwordForm.get('newPassword').hasError('pattern') ? 'You must input a valid password' :
+      this.passwordForm.get('newPassword').hasError('ConfirmPassword') ? 'Passwords match' : '';
+  }
 
   ngOnInit() {
   }
