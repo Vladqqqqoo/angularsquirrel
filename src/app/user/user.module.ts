048e10bb734984e42eaaaf8cfe0b24814ad4f469
Sergei <s.belinski.vironit@vironit.ru>
2019-04-25 18:37:04 +0300

    style : change font size

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './components/user/user.component';
 import {UserRoutingModule} from './user-routing.module';
-import {MatNavList, MatSidenavModule, MatListModule} from '@angular/material';
+import {MatNavList, MatSidenavModule, MatListModule, MatFormFieldModule} from '@angular/material';
 import { UserInfoComponent } from './components/user-info/user-info.component';
 import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 import { PasswordComponent } from './components/password/password.component';
@@ -14,7 +14,8 @@ import { SocialNetworksComponent } from './components/social-networks/social-net
     CommonModule,
     UserRoutingModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule
   ]
 })
 export class UserModule { }
