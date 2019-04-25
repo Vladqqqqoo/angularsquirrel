da5bdc33e6d79a1f324b0ead6482128fc5808bc3
Sergei <s.belinski.vironit@vironit.ru>
2019-04-25 16:55:55 +0300

    feat: add user page routing

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserComponent } from './componenst/user.component';
+import { UserComponent } from './components/user/user.component';
 import {UserRoutingModule} from './user-routing.module';
+import {MatNavList, MatSidenavModule, MatListModule} from '@angular/material';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
+import { PasswordComponent } from './components/password/password.component';
+import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserInfoComponent, MyProjectsComponent, PasswordComponent, SocialNetworksComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class UserModule { }
