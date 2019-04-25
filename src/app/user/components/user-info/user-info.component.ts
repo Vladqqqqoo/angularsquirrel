8f8a5680d0c61ceea09e805c28547854deb6bba6
Vladislav <v.drobovich.vironit@vironit.ru>
2019-04-25 20:06:21 +0300

    feat: make form for user info

--- src/app/user/components/user-info/user-info.component.ts
+++ src/app/user/components/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-info',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = fb.group({
+      firstName: ['', [Validators.required, Validators.pattern(/^[а-яА-ЯёЁa-zA-Z]+$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[а-яА-ЯёЁa-zA-Z]+$/)]],
+      age: [null, [Validators.required, Validators.pattern(/(^[1-9][0-9]?$)|(^100$)/)]],
+      email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+    })
+  }
+
+  getFirstNameErrorMessage() {
+    return this.userForm.get('firstName').hasError('required') ? 'You must enter a value' :
+      this.userForm.get('firstName').hasError('pattern') ? 'Not a valid first name' :
+        '';
+  }
 
   ngOnInit() {
   }
