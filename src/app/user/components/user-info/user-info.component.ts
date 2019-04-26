861165b93ea03d83b13a771da27d1e86e2fecb81
Vladislav <v.drobovich.vironit@vironit.ru>
2019-04-26 22:03:27 +0300

    feat: add ng2-file-upload, make uploading avatar for user-info page

--- src/app/user/components/user-info/user-info.component.ts
+++ src/app/user/components/user-info/user-info.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
+
+
+import { FileUploader } from 'ng2-file-upload';
+
+const URL = 'my-backend.com/file-upload';
+
+
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.scss']
+  styleUrls: ['./user-info.component.scss'],
 })
 export class UserInfoComponent implements OnInit {
 
+  @ViewChild('fileInput') fileInput;
   userForm: FormGroup;
-  uploadForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private httpClient: HttpClient) {
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.userForm = fb.group({
       login: ['', [Validators.required, Validators.pattern(/^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/)]],
       email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
@@ -21,7 +38,7 @@ export class UserInfoComponent implements OnInit {
       age: [null, [Validators.pattern(/(^[1-9][0-9]?$)|(^100$)/)]],
       location: ['', [Validators.pattern(/^[#.0-9a-zA-Z\s,-]+$/)]],
       website: ['', [Validators.pattern(/^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/)]],
-      skills: ['',[Validators.pattern(/[а-яА-ЯёЁa-zA-Z,]+/)]],
+      skills: ['', [Validators.pattern(/[а-яА-ЯёЁa-zA-Z,]+/)]],
       bio: ['',],
     })
   }
@@ -32,28 +49,17 @@ export class UserInfoComponent implements OnInit {
         '';
   }
 
-  onFileSelect(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.uploadForm.get('profile').setValue(file);
-    }
+  clearUpload() {
+    this.uploader.clearQueue();
+    this.uploader.cancelAll();
   }
 
+  onUpload() {
+    this.uploader.uploadAll();
+  }
 
   ngOnInit() {
-    this.uploadForm = this.fb.group({
-      profile: ['']
-    });
-  }
 
-  onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.uploadForm.get('profile').value);
-
-    this.httpClient.post<any>('http://localhost:3000/customers/login', formData).subscribe(
-      (res) => console.log(res),
-      (err) => console.log(err)
-    );
   }
 
 }
