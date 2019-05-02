f2bae31439b06912de3c8483a2d3996bb7017272
Vladislav <v.drobovich.vironit@vironit.ru>
2019-05-02 15:27:24 +0300

    feat: make guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   {path: '', loadChildren: './main/main.module#MainModule'},
-  {path: 'account', canLoad: [], loadChildren: './user/user.module#UserModule'},
-  {path: 'profile', canLoad: [], loadChildren: './user-view/user-view.module#UserViewModule'},
-  {path: 'shots', canLoad: [], loadChildren: './shots/shots.module#ShotsModule'},
+  {path: 'account', canLoad: [AuthGuard], canActivate: [AuthGuard], loadChildren: './user/user.module#UserModule'},
+  {path: 'profile', canLoad: [AuthGuard], canActivate: [AuthGuard], loadChildren: './user-view/user-view.module#UserViewModule'},
+  {path: 'shots', canLoad: [AuthGuard], canActivate: [AuthGuard], loadChildren: './shots/shots.module#ShotsModule'},
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment, UrlTree
+} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "./share/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad, CanActivate{
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+}
